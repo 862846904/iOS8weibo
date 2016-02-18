1797df33d0fa71be1bf8f8012762c72c6475648c
lp <lp@itcast.com>
2016-02-18 20:56:46 +0800

    初始化项目

---
+++ iOS8weibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS8weibo
+//
+//  Created by Apple on 16/2/18.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
